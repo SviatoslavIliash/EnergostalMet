bf75202ade9047663e28754e7be583ad57f7fbba
SviatoslavIliash <iliashsviatoslav@gmail.com>
2023-12-21 16:07:33 +0200

    Added models for product and category

--- energosite/store/models.py
+++ energosite/store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    #attributes = models.
+    image = models.ImageField(upload_to='uploads/products/')
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
