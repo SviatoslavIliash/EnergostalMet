90790b0c5fde60f38a2d758eb9976961786a9150
SviatoslavIliash <iliashsviatoslav@gmail.com>
2023-12-21 17:17:37 +0200

    Added attributes to models for product

--- energosite/store/models.py
+++ energosite/store/models.py
@@ -12,6 +12,16 @@ class Product(models.Model):
     name = models.CharField(max_length=30)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    #attributes = models.
+    attributes = models.ManyToManyField("Attribute", through="ProductAttrs")
     image = models.ImageField(upload_to='uploads/products/')
     description = models.CharField(max_length=250, default='', blank=True, null=True)
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class ProductAttrs(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    value = models.CharField(max_length=50)
