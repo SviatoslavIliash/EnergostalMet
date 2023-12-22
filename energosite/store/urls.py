7cea984b694ea1531aa24d3babaa86520174a215
KarolinaIliash <karolina.iliash@gmail.com>
2023-12-22 02:08:38 +0200

    Added initial view and template for Product.

--- energosite/store/urls.py
+++ energosite/store/urls.py
@@ -6,4 +6,5 @@ app_name = "store"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:category_name>/", views.category_detail, name="category_detail"),
+    path("<int:product_id>/product/", views.product_detail, name="product_detail"),
 ]
--- energosite/store/views.py
+++ energosite/store/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Category, Product
+from .models import Category, Product, ProductAttrs
 
 
 def index(request):
@@ -12,3 +12,10 @@ def category_detail(request, category_name):
     product_list = Product.objects.filter(category__name=category_name)
     context = {"product_list" : product_list, "category_name" : category_name}
     return render(request, "store/category.html", context)
+
+def product_detail(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    attributes = ProductAttrs.objects.filter(product=product)
+    context = {"product": product, "attrs": attributes}
+    return render(request, "store/product.html", context)
+
