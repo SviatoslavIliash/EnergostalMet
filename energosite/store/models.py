b262ed38403ffcfa8338b22ff47ddc36574e0715
KarolinaIliash <karolina.iliash@gmail.com>
2023-12-28 12:52:04 +0200

    Made product description non-nullable with default empty value. Empty description now is empty string, not None.

--- energosite/store/models.py
+++ energosite/store/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     attributes = models.ManyToManyField("Attribute", through="ProductAttrs")
     image = models.ImageField(upload_to='images/')
-    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    description = models.CharField(max_length=250, default='', blank=True)
 
     def __str__(self):
         return self.name
