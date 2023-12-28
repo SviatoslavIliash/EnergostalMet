e907452f1a90b824ecffe7a91808c0632b146239
KarolinaIliash <karolina.iliash@gmail.com>
2023-12-28 12:41:07 +0200

    Added attributes to Product page in Admin site.

--- energosite/store/admin.py
+++ energosite/store/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from .models import Category, Product, Attribute, ProductAttrs
 # Register your models here.
 
+class ProductAttrsInline(admin.TabularInline):
+    model = ProductAttrs
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductAttrsInline,
+    ]
+
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Attribute)
-admin.site.register(ProductAttrs)
+
