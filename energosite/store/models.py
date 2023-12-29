c8cce6072aeab45f43570e9fd6a02787eb8d4aa0
SviatoslavIliash <iliashsviatoslav@gmail.com>
2023-12-29 23:17:22 +0200

    Support for empty img aligned rows in category

--- energosite/store/models.py
+++ energosite/store/models.py
@@ -1,32 +1,51 @@
 from django.db import models
+from django.templatetags.static import static
+
 # Create your models here.
+
+
+# Move this func to some util file
+def get_image_impl(obj):
+    if obj.image:
+        return obj.image.url
+    else:
+        return static('store/images/default_image.jpg')
 
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
     parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
-    description = models.CharField(max_length=250, default='', blank=True, null=True)
-    image = models.ImageField(upload_to='images/', default='')
+    description = models.CharField(max_length=250, default='', blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+
+    def get_image(self):
+        return get_image_impl(self)
 
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=30)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     attributes = models.ManyToManyField("Attribute", through="ProductAttrs")
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
     description = models.CharField(max_length=250, default='', blank=True)
+
+    def get_image(self):
+        return get_image_impl(self)
 
     def __str__(self):
         return self.name
+
 
 class Attribute(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
+
 
 class ProductAttrs(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
