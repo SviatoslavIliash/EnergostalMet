38125d5029daa1292d7e1306325a910c48b16452
KarolinaIliash <karolina.iliash@gmail.com>
2024-01-02 22:48:38 +0200

    Added name search field to admin Categories page.

--- energosite/store/admin.py
+++ energosite/store/admin.py
@@ -13,7 +13,11 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ["name", "category__name"]
 
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Attribute)
 admin.site.register(Article)
