1ee7694c7e61f1dd8f0816513894dcb79c2dbfa8
SviatoslavIliash <iliashsviatoslav@gmail.com>
2024-01-12 23:19:10 +0200

    Fixed update form in cart

--- energosite/cart/forms.py
+++ energosite/cart/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.IntegerField(initial=1, min_value=1, step_size=1, widget=forms.NumberInput(attrs={"class": "input_quantity"}))
+    quantity = forms.IntegerField(label=False, initial=1, min_value=1, required=True, step_size=1,
+                                  widget=forms.NumberInput(
+                                      attrs={"class": "input_quantity"}))
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
 
